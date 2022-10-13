c97e646232149cc79fe604c76783bc61ac3fb762
inna <inna.obraztsovas@gmail.com>
2022-10-13 16:45:10 +0300

    transform routes

--- gateway/src/Controller/APIController.php
+++ gateway/src/Controller/APIController.php
@@ -18,6 +18,7 @@ class APIController extends AbstractController
     public function register(Request $request)
     {
         $data = json_decode($request->getContent(), flags: JSON_THROW_ON_ERROR);
+        $routes = $this->transform($data);
         $cache = md5($data->service_name);
         if ($this->cachePool->existCache($cache)) {
             $msg = 'Already exist';
@@ -27,11 +28,25 @@ class APIController extends AbstractController
             $this->cachePool->save($cache, (array)$data->routes);
         }
         return $this->json(['msg' => $msg], 201);
+    }
 
+    private function transform(object $data): array
+    {
+        $routes = [];
+        foreach ($data->routes as $route) {
+            if (!empty($route->methods)) {
+                foreach ($route->methods as $method) {
+                    $routes[$method] = [
+                        $route->path => $data->service_name
+                    ];
+                }
+            }
+        }
+        return $routes;
     }
 
 
-    #[Route(path: '/{slug}', name: 'entrypoint', requirements: ['slug' => '.*'], methods: 'ANY')]
+    #[Route(path: '/{slug}', name: 'entrypoint', requirements: ['slug' => '.*'])]
     public function handle(Request $request)
     {
       $uri = json_encode($request->getRequestUri());
