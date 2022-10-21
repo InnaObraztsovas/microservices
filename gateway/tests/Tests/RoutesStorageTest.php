06d59c750321119d82db2ada4794ee9f35c251f0
inna <inna.obraztsovas@gmail.com>
2022-10-21 15:57:17 +0300

    code refactoring, unit tests

--- gateway/tests/Tests/RoutesStorageTest.php
+++ gateway/tests/Tests/RoutesStorageTest.php
@@ -1,32 +1,63 @@
 <?php
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RequestContext;
 
 
-class RoutsStorageTest extends KernelTestCase
+class RoutesStorageTest extends KernelTestCase
 {
 
     public function testIfRouteStore()
     {
         $cachePool = new \App\Storage\CachePool();
         $mock = $this->createMock(\App\Storage\CachePool::class);
-
-        $serviceName = 'users';
+        $serviceName = 'test';
         $existingRoutes = $cachePool->get();
+
         $this->assertIsArray($existingRoutes);
         $this->assertIsString($serviceName);
+        $this->assertArrayHasKey($serviceName, $existingRoutes);
 
         $mock
             ->method('delete')
             ->willReturnSelf();
+
         $mock
             ->method('save')
             ->with($existingRoutes)
             ->willReturnSelf();
     }
 
-//    public function testIfRoutMatch()
-//    {
-//        $collection = new \Symfony\Component\Routing\RouteCollection();
-//    }
+    public function testIfRoutMatch()
+    {
+        $collection = $this->createMock(\Symfony\Component\Routing\RouteCollection::class);
+        $cachePool = new \App\Storage\CachePool();
+        $existingRoutes = $cachePool->get();
+        $this->assertIsArray($existingRoutes);
+
+        foreach ($existingRoutes as $routes) {
+            foreach ($routes as $name => $route) {
+                $this->assertIsString($route->getHost());
+                $hosts[$name] = $route->getHost();
+
+                $route->setHost(null);
+
+                $collection->method('add')
+                    ->with($name, $route);
+
+            }
+        }
+
+        $requestContext = $this->createMock(RequestContext::class);
+        $request = new Request(); // а як мені його отримати?
+
+        $urlMatcher = new \Symfony\Component\Routing\Matcher\UrlMatcher($collection, $requestContext);
+        $match = $urlMatcher->matchRequest($request);
+        $this->assertIsArray($match);
+        $collection->method('get')
+            ->with($match['_route'])
+            ->willReturn(\Symfony\Component\Routing\Route::class);
+
+    }
 }
